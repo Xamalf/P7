b2e60c2394fe0ecd41a7da6f85b568b9c8a3d521
Xamalf <casperbis1999@gmail.com>
2022-11-25 14:45:38 +0100

    Added api for available exercises

--- exercise-provider/exercise-provider.py
+++ exercise-provider/exercise-provider.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException, Response
 import requests
 import uvicorn
 from pydantic import BaseModel
+import glob
 
 app = FastAPI(root_path="/exercise-provider")
 
@@ -56,5 +57,27 @@ async def root(exercise_id: Exercise_id):
     )
 
     return jsonObject
+
+@app.post("/exercise-provider/avalable-exercises")
+async def root():
+    try:
+        auth_response = requests.post("http://auth.default:3000/auth", json={"name": "tester"}) # Calling auth
+        # exercise_response = await data_base # Calling exercise database
+        if auth_response.status_code != 200:
+            raise HTTPException(
+                status_code=404,
+                detail="Auth failed"
+            )
+    except Exception:
+        raise HTTPException(
+            status_code=404,
+            detail="Auth failed"
+        )
+
+    files = glob.glob("./exercises/*.xml")
+
+    files = [filename[12:-4] for filename in files]
+
+    return files
 
 uvicorn.run(app, host="0.0.0.0", port=2000)
